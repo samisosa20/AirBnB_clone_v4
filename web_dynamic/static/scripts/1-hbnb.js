9647dbb2cf4f020e1ab8e1dd5952448149404b62
KaroDev <karodev3@gmail.com>
2020-09-28 12:08:12 +0000

    Fix pep8 and semistandard, Update AUTHORS

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,11 @@
 $(document).ready(() => {
-	let amenityId = {};
-	$(".amenities INPUT").change(function() {
-		if (this.checked) {
-			amenityId[$(this).attr("data-id")] = $(this).attr("data-name");
-		} else {
-			delete amenityId[$(this).attr("data-id")];
-		}
-		$(".amenities H4").html(Object.values(amenityId).join(', '));
-	});
+  const amenityId = {};
+  $('.amenities INPUT').change(function () {
+    if (this.checked) {
+      amenityId[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenityId[$(this).attr('data-id')];
+    }
+    $('.amenities H4').html(Object.values(amenityId).join(', '));
+  });
 });
